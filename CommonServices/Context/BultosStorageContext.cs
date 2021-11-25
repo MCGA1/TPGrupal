514d868cdab0496a4995da71b8019bf1eef5530d
Alan Javier Rada <AlanJavier.Rada@Alumnos.uai.edu.ar>
2021-11-25 20:43:06 -0300

    add dbcontext

---
+++ CommonServices/Context/BultosStorageContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Prensa.Control;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonServices.Context
+{
+    public class BultosStorageContext : DbContext
+    {
+
+        public BultosStorageContext(DbContextOptions<BultosStorageContext> options)
+         : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-JP7JEOE;Initial Catalog=BultosStorage;Integrated Security=True");
+        }
+
+        public DbSet<BultoProcesado> BultoProcesados { get; set; }
+
+
+
+    }
+}
