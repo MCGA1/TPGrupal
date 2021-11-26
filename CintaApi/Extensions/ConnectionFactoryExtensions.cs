12e52f4e03bcd7e33c498b90932ae3a04814b627
Ezequiel Silva Hevia <ezesil96@gmail.com>
2021-11-26 15:06:37 -0300

    Fix referencias de cinta

---
+++ CintaApi/Extensions/ConnectionFactoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CintaApi.Extensions
+{
+    public static  class ConnectionFactoryExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static ConnectionFactory ConnectionFactory()
+        {
+            return new ConnectionFactory() { HostName = "localhost",DispatchConsumersAsync=true };
+        }
+    }
+}
